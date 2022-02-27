b12265dd638724c9bcec1cc71adf4bb307cfe24b
David Borzek <mail@davidborzek.de>
2022-02-27 12:47:46 +0100

    feat: implement play pause button

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,25 @@ export = (api: PluginApi) => {
   start();
 };
 
+const PLAY_PAUSE_BUTTON_PIN = 15;
+
 const start = () => {
   const logger = spoddifyMopped.getLogger('spoddify-mopped-gpio');
 
   logger.info('GPIO service started.');
+
+  const playPauseButton = new onoff.Gpio(
+    PLAY_PAUSE_BUTTON_PIN,
+    'in',
+    'falling'
+  );
+
+  playPauseButton.watch((err) => {
+    if (err) {
+      logger.error(`GPIO: Play pause button failed with: ${err}`);
+      return;
+    }
+
+    spoddifyMopped.playPause();
+  });
 };
