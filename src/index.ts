89b1ca3520d6ad8aff453d3bf43b4eb05bf7a056
David Borzek <mail@davidborzek.de>
2022-02-27 13:40:57 +0100

    feat: get pin config from spoddify-mopped plugin config

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,34 @@ import { PluginApi } from 'spoddify-mopped';
 import onoff from 'onoff';
 
 let spoddifyMopped: PluginApi;
+let config: Config | undefined;
 
-export = (api: PluginApi) => {
+type Config = {
+  playPausePin?: number;
+  nextPin?: number;
+  previousPin?: number;
+};
+
+export = (api: PluginApi, _config?: Config) => {
   spoddifyMopped = api;
+  config = _config;
   start();
 };
 
-const PLAY_PAUSE_BUTTON_PIN = 15;
-const NEXT_BUTTON_PIN = 14;
-const PREVIOUS_BUTTON_PIN = 10;
-
 const start = () => {
   const logger = spoddifyMopped.getLogger('spoddify-mopped-gpio');
+
+  if (!config || Object.keys(config).length < 1) {
+    logger.warn('GPIO: skipping - no config found.');
+    return;
+  }
+
   logger.info('GPIO service started.');
 
-  if (PLAY_PAUSE_BUTTON_PIN) {
+  if (config?.playPausePin) {
     logger.info('GPIO: Play pause button enabled.');
     const playPauseButton = new onoff.Gpio(
-      PLAY_PAUSE_BUTTON_PIN,
+      config.playPausePin,
       'in',
       'falling'
     );
@@ -34,9 +44,9 @@ const start = () => {
     });
   }
 
-  if (NEXT_BUTTON_PIN) {
+  if (config?.nextPin) {
     logger.info('GPIO: Next button enabled.');
-    const nextButton = new onoff.Gpio(NEXT_BUTTON_PIN, 'in', 'falling');
+    const nextButton = new onoff.Gpio(config.nextPin, 'in', 'falling');
 
     nextButton.watch((err) => {
       if (err) {
@@ -48,9 +58,9 @@ const start = () => {
     });
   }
 
-  if (PREVIOUS_BUTTON_PIN) {
+  if (config?.previousPin) {
     logger.info('GPIO: Previous button enabled.');
-    const previousButton = new onoff.Gpio(PREVIOUS_BUTTON_PIN, 'in', 'falling');
+    const previousButton = new onoff.Gpio(config.previousPin, 'in', 'falling');
 
     previousButton.watch((err) => {
       if (err) {
