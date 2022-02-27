5820e202de32fcdb4bfc0d7c6f62fcb3af139e7b
David Borzek <mail@davidborzek.de>
2022-02-27 11:18:41 +0100

    chore: add spoddify-mopped types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
+import { PluginApi } from 'spoddify-mopped';
 import onoff from 'onoff';
 
-let spoddifyMopped;
+let spoddifyMopped: PluginApi;
 
-export = (api) => {
+export = (api: PluginApi) => {
   spoddifyMopped = api;
-
   start();
 };
 
-const start = () => {};
+const start = () => {
+  const logger = spoddifyMopped.getLogger('spoddify-mopped-gpio');
+
+  logger.info('GPIO service started.');
+};
